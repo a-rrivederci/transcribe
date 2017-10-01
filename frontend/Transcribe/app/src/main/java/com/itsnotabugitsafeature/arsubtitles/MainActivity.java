e05c189737c04a05ed772a5f49ad0d426166e82d
a-arshad <a-arshad@users.noreply.github.com>
2017-10-01 02:28:07 -0700

    Recieves message and properly displays :)

--- frontend/Transcribe/app/src/main/java/com/itsnotabugitsafeature/arsubtitles/MainActivity.java
+++ frontend/Transcribe/app/src/main/java/com/itsnotabugitsafeature/arsubtitles/MainActivity.java
@@ -4,6 +4,7 @@ import android.hardware.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.WindowManager;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 import com.github.nkzawa.socketio.client.IO;
@@ -18,22 +19,27 @@ public class MainActivity extends AppCompatActivity {
     private CameraView mCameraView = null;
 
     private String TAG = "Transcribe";
+    TextView helloTextView;
+
+
 
     private Socket mSocket;
     {
         try {
+            //try to connect to the socket
             mSocket = IO.socket("https://faa580b6.ngrok.io");
         } catch (URISyntaxException e) {}
+
     }
 
+    //listener for a message emitted from server
     private Emitter.Listener onNewMessage = new Emitter.Listener() {
         @Override
         public void call(final Object... args) {
-            MainActivity.this.runOnUiThread(new Runnable() {
+            runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
-                    // args[0] will be the string
-                    Log.d(TAG, (String) args[0]);
+                    helloTextView.setText((String) args[0]);
                 }
             });
         }
@@ -43,15 +49,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        mSocket.on("new message", onNewMessage);
+        //connect to socket server
+
+        mSocket.on("app_captions", onNewMessage);
         mSocket.connect();
 
+        Log.i(TAG, "Connected to Server.");
+
+        //display junk onto phone maybe?
         setContentView(R.layout.activity_main);
 
-        //STRING
-        final TextView helloTextView = (TextView) findViewById(R.id.text_view_id);
+        //Prevents phone from locking on its own
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        //draw string onto screen
+        helloTextView = (TextView) findViewById(R.id.text_view_id);
         helloTextView.setText(R.string.transcribedText);
 
+        //open camera junk
         try {
             mCamera = Camera.open();//you can use open(int) to use different cameras
         } catch (Exception e) {
@@ -67,11 +82,12 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    //disconnect from server to prevent leaks
     @Override
     public void onDestroy() {
         super.onDestroy();
 
         mSocket.disconnect();
-        mSocket.off("new message", onNewMessage);
+        mSocket.off("app_captions", onNewMessage);
     }
 }
