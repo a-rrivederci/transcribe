03ce6e83473fb247e0b2c3461c86c7f746fdd708
a-arshad <a-arshad@users.noreply.github.com>
2017-09-30 18:25:11 -0700

    adding frontend mobile component

---
+++ frontend/Transcribe/app/src/main/java/com/itsnotabugitsafeature/arsubtitles/MainActivity.java
@@ -0,0 +1,37 @@
+package com.itsnotabugitsafeature.arsubtitles;
+
+import android.hardware.Camera;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Camera mCamera = null;
+    private CameraView mCameraView = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        try{
+            mCamera = Camera.open();//you can use open(int) to use different cameras
+        } catch (Exception e){
+            Log.d("ERROR", "Failed to get camera: " + e.getMessage());
+        }
+
+        if(mCamera != null) {
+            mCameraView = new CameraView(this, mCamera);//create a SurfaceView to show camera data
+            FrameLayout camera_view = (FrameLayout)findViewById(R.id.camera_view);
+            camera_view.addView(mCameraView);//add the SurfaceView to the layout
+        }
+
+
+    }
+
+
+}
