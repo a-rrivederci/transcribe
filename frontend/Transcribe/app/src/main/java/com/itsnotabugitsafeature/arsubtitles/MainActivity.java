5a2325faf7f07aa01c1a297550223b5db2a8479d
a-arshad <a-arshad@users.noreply.github.com>
2017-10-01 12:03:10 -0700

    Another URL update

--- frontend/Transcribe/app/src/main/java/com/itsnotabugitsafeature/arsubtitles/MainActivity.java
+++ frontend/Transcribe/app/src/main/java/com/itsnotabugitsafeature/arsubtitles/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
     {
         try {
             //try to connect to the socket
-            mSocket = IO.socket("https://567334d1.ngrok.io");
+            mSocket = IO.socket("https://300af425.ngrok.io/");
         } catch (URISyntaxException e) {}
 
     }
