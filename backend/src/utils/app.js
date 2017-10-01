c46b256cb644d053a0499eee7884ebd5581ce9a7
Elvis Eshikena <elviseshikena@gmail.com>
2017-09-30 22:56:04 -0700

    added product template page

--- backend/src/utils/app.js
+++ backend/src/utils/app.js
@@ -23,6 +23,9 @@ async function start() {
     sharedInstance.io.on('connection', (socket) => { 
         console.log('Client Connected!');
         socket.on('voice_captions', (voiceCaptions) => {
+            socket.emit('app_captions', voiceCaptions);
+        });
+        socket.on('voice_captions', (voiceCaptions) => {
             console.log(voiceCaptions);
         });
     });
